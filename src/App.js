afad865fe752a405ecd65fdd037d7d0b95ee8553
Arifhusain Soudagar <arifhusainsoudagar@gmail.com>
2024-08-25 10:41:36 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import "./App.css";
-import CardWrapper from "./Components/CardWrapper/CardWrapper";
-import Header from "./Components/Header/Header";
-import Hero from "./Components/Hero/Hero";
 
 function App() {
   return (
     <>
-      <Header />
-      <Hero />
-      <CardWrapper />
+    <h1>hello</h1>
     </>
   );
 }
