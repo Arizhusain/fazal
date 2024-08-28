5c097284a477b84f2599c819878fe2eae56bc107
Arifhusain Soudagar <arifhusainsoudagar@gmail.com>
2024-08-28 23:30:46 +0530

    add product page

--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -6,6 +6,9 @@ import { colors, sizes } from '../../demo'
 
 const ProductDetails = () => {
     const [size, setSize] = useState('');
+    const [quantity, setQuantity] = useState(1);
+    const maxSize = 10;
+
     const handleClick = (selectedSize) => {
         setSize(selectedSize);
     }
@@ -15,6 +18,11 @@ const ProductDetails = () => {
                 <div className='product-list-container'>
                     <div className='product-list-filter'>
                         <div className='productDetails-image-container'>
+                            <div className='productDetails-image-wrapper'>
+                                <img className='productDetails-image-wrapper-innerimg' src="https://cdn-media.powerlook.in/catalog/product/d/p/dp101-1030910.jpg?aio=w-384" alt="" />
+                                <img className='productDetails-image-wrapper-innerimg' src="https://cdn-media.powerlook.in/catalog/product/d/p/dp101-1030910.jpg?aio=w-384" alt="" />
+                                <img className='productDetails-image-wrapper-innerimg' src="https://cdn-media.powerlook.in/catalog/product/d/p/dp101-1030910.jpg?aio=w-384" alt="" />
+                            </div>
                             <img src="https://cdn-media.powerlook.in/catalog/product/d/p/dp101-1030910.jpg?aio=w-384" alt="" />
                         </div>
                     </div>
@@ -22,23 +30,38 @@ const ProductDetails = () => {
                         <h1 className='productDetails-header'>Pista Regular Fit Half Sleeves T-Shirt</h1>
                         <h2 className='productDetails-header-price'>₹699</h2>
                         <p className='productDetails-header-tag'>Inclusive of all taxes</p>
-                        <div className='productDetails-header-label'>More Colors</div>
-                        <ul className='productDetails-header-colors'>
-                            {
-                                colors.map((item) => (
-                                    <CheckboxColorIcon item={item} />
-                                ))
-                            }
-                        </ul>
-                        <div className='productDetails-header-label'>Select Size</div>
-                        <ul className='productDetails-header-colors'>
-                            {
-                                sizes.map((item) => (
-                                    <SizeBox item={item} size={size} onSizeSelect={handleClick} />
-                                ))
-                            }
-                        </ul>
-                        <div className='productDetails-header-label'>Quantity</div>
+                        <div className='productDetails-padding-bottom'>
+                            <div className='productDetails-header-label'>More Colors</div>
+                            <ul className='productDetails-header-colors'>
+                                {
+                                    colors.map((item) => (
+                                        <CheckboxColorIcon item={item} />
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                        <div className='productDetails-padding-bottom'>
+                            <div className='productDetails-header-label'>Select Size</div>
+                            <ul className='productDetails-header-colors'>
+                                {
+                                    sizes.map((item) => (
+                                        <SizeBox item={item} size={size} onSizeSelect={handleClick} />
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                        <div className='productDetails-padding-bottom'>
+                            <div className='productDetails-header-label'>Quantity</div>
+                            <div className='productDetails-button-wrapper'>
+                                <button className='productDetails-button' disabled={quantity === 1} onClick={() => setQuantity(pre => pre - 1)}>-</button>
+                                <span className='productDetails-header-label'>{quantity}</span>
+                                <button className='productDetails-button' disabled={quantity === maxSize} onClick={() => setQuantity(pre => pre + 1)}>+</button>
+                            </div>
+                        </div>
+                        <div className='productDetails-button-wrapper'>
+                            <button className='productDetails-wishlist-button'>Add to wishlist</button>
+                            <button className='productDetails-addtocart-button'>Add to cart</button>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/Pages/SingleProductPage.jsx
+++ src/Pages/SingleProductPage.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ProductDetails from '../Components/ProductDetails/ProductDetails'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
+import Features from '../Components/Features/Features'
+import ProductList from '../Components/ProductList/ProductList'
+import CategoryHeading from '../Components/CategoryHeading/CategoryHeading'
 
 const SingleProductPage = () => {
     return (
         <>
             <ProductDetails />
+            <div style={{ textAlign: 'center' }}>
+                <CategoryHeading categoryHeading="Similar products" />
+            </div>
+            <ProductList />
+            <Features />
+            <NewsLetter />
         </>
     )
 }
